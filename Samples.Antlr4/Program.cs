a79990335aa51e994895393048723054b9616289
avit sidis <avit.sidis@gmail.com>
2018-01-13 17:17:17 +0100

    Add project files.

---
+++ Samples.Antlr4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples.Antlr4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
